5335b602c03a94ef98cc9f9c69b7c7124cfca7f5
Josh Kardos <joshkardos@gmail.com>
2020-03-02 14:51:16 -0800

    added priv policy and support:

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import logo from './logo.png';
-import DownloadOnAppStore from './DownloadOnAppStore.png';
-import './App.css';
+import MainScreen from './MainScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Flix Clothing </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={DownloadOnAppStore} className="Download" alt="logo" />
-      </header>
-    </div>
+    <MainScreen />
   );
 }
 
